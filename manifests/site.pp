c02f00a1dd72d0a14a82d412b556ed31b7c62887
miromir237 <39900813+miromir237@users.noreply.github.com>
2018-06-26 14:02:34 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,8 @@ node default {
     content => 'This is a README',
     owner => 'root',
   }
+  file {'/root/README2':
+    ensure => file,
+    content => 'This is second README',
+    owner => 'root',
 }
